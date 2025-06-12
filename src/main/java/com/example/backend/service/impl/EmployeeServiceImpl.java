d7889faf73011b39b1f3d9b0b40d1cfd9a2a36cf
Thimira Vimukthi <ictthimira@gmail.com>
2025-06-12 16:15:54 +0530

    create log

--- src/main/java/com/example/backend/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/example/backend/service/impl/EmployeeServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.backend.dto.EmployeeDTO;
 import com.example.backend.entity.Employee;
 import com.example.backend.repo.EmployeeRepo;
 import com.example.backend.service.EmployeeService;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
     @Autowired
@@ -19,11 +21,12 @@ public class EmployeeServiceImpl implements EmployeeService {
     private ModelMapper mapper;
 
     public void save(EmployeeDTO dto) {
+        log.info("save EmployeeDTO");
         repo.save(mapper.map(dto, Employee.class));
     }
 
     public List<EmployeeDTO> getAll() {
-        System.out.println("hi hi...");
+
         return repo.findAll().stream()
                 .map(e -> mapper.map(e, EmployeeDTO.class))
                 .collect(Collectors.toList());
